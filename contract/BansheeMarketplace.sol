9f0466e2ca199e5998c433da72e2ecfb5700e55e
Lamont V. <lamontsprin@aol.com>
2024-09-02 11:28:20 -0400

    Update BansheeMarketplace.sol

--- contract/BansheeMarketplace.sol
+++ contract/BansheeMarketplace.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
-
+// Marketplace Smart Contract minting ERC721 tokens
 contract BansheeMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter public _tokenIds;
